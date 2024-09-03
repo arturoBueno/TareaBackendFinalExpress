ea037b386a5037bf5a4e60ec5eee71ed8c2b6fa1
arturoBueno <jabuenor@unac.edu.co>
2024-09-03 00:08:49 -0500

    Añadir archivos y actualizar .gitignore

---
+++ index.js
@@ -0,0 +1,30 @@
+// import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+// import usersRouter from "./routes/users.routes";
+// import { dbConnection } from "./config/db";
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser());
+
+app.get('/', (req, res) => {
+    res.send('¡Bienvenido a la API!');
+});
+
+// app.use("/users", usersRouter);
+
+try {
+    dbConnection.authenticate();
+    console.log("Connected to DB");
+} catch (error) {
+    console.log(error);
+}
+
+app.listen(8080, () => {
+    console.log("Server running on http://localhost:8080");
+});
+
+
